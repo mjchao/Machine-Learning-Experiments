1cfb0eb1bfac8dafcd2ef75c1361974966629a53
Mickey Chao <mjchao2@gmail.com>
2015-07-08 23:15:16 -0400

    Improve some documentation of linear regression

--- Linear Regression/multivariate/generalized/run.m
+++ Linear Regression/multivariate/generalized/run.m
@@ -1,6 +1,17 @@
 function [] = run( thetas , m , noiseSize )
-%RUN runs this version of multivariate linear regression for the
-%general case
+%RUN Runs this version of multivariate linear regression for the
+% general case. It generates data points using a given linear
+% relationship, applies some noise to it, then uses multivariate
+% linear regression to try to determine the origin linear relation.
+%
+% thetas are the real coefficients of some relation:
+% y = theta0*x_0 + theta_1*x_1 + theta_2*x_2 + ... + theta_n*x_n
+% where x_0, ..., x_n are independent variables and x_0 = 1 always
+%
+% m is the number of data points to generate. More data points will
+% probably yield better regression results
+%
+% noiseSize is the magnitude of the noise to add to the datapoints.
 [ x , y ] = genData( thetas , m , noiseSize );
 
 n = size( thetas , 2 );
--- Linear Regression/multivariate/generalized/run3D.m
+++ Linear Regression/multivariate/generalized/run3D.m
@@ -1,6 +1,7 @@
 function [] = run3D( thetas , m , noiseSize )
 %RUN runs this version of multivariate linear regression with 2 
-% independent variables
+% independent variables. The data points and approximation are graphed
+% and shown.
 [ x , y ] = genData( thetas , m , noiseSize );
 approxThetas = gradientDescent( [ 0 , 0 , 0 ] , x , y );
 graph( x , y , approxThetas );
