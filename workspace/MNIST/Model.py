f5af04084c865137566b31744c1d1563676a955f
Mickey Chao <mjchao2@gmail.com>
2016-07-04 14:21:19 -0700

    Implement model and training

--- workspace/MNIST/Model.py
+++ workspace/MNIST/Model.py
@@ -12,11 +12,108 @@ from tensorflow.examples.tutorials.mnist import input_data
 WIDTH = 28
 HEIGHT = 28
 
-class Learner(object):
+class SimpleLearner(object):
+    """A simple digit recognizer that applies softmax regression.
+    """
     def __init__(self):
+        """Sets up the graph for softmax regression.
+        """
         self.x_ = tf.placeholder(tf.float32, [None, WIDTH*HEIGHT])
+        self.W_ = tf.Variable(tf.zeros([WIDTH*HEIGHT, 10]))
+        self.b_ = tf.Variable(tf.zeros([10]))
+        init = tf.initialize_all_variables()
+        self.sess_ = tf.Session()
+        self.sess_.run(init)
+
+        # We want out prediction to be n rows of 10 probabilities (the
+        # probability of being each digit). Therefore, we need to multiply
+        # x (n x 784) by W (784 x 10) so that our output will have dimension
+        # (n x 10).
+        self.y_ = tf.nn.softmax(tf.matmul(self.x_, self.W_) + self.b_)
+
+    def Train(self, train_data, batch_size=100, num_iters=1000):
+        """Trains the model.
+        
+        Args:
+            train_data: The training data. It must provide a function next_batch
+                that allows us to get a training batch for batch SGD.
+            batch_size: Size of the training batch.
+            num_iters: Number of iterations for which to train.
+        """
+        # Use cross entropy loss to evaluate the performance of the softmax
+        # regression.
+        y_true = tf.placeholder(tf.float32, [None, 10])
+        cross_entropy_loss = tf.reduce_mean(-tf.reduce_sum(
+                        y_true*tf.log(self.y_), reduction_indices=[1]))
+        train_step = tf.train.GradientDescentOptimizer(0.5).minimize(
+                                                    cross_entropy_loss)
+
+        for _ in range(num_iters):
+            batch_features, batch_labels = train_data.next_batch(batch_size)
+            self.sess_.run([train_step], 
+                     {self.x_: batch_features, y_true: batch_labels})
+
+    def Predict(self, features):
+        """Predicts what digit the given data represents.
+        """
+        prediction = tf.argmax(self.y_, 1)
+        return self.sess_.run(prediction, {self.x_: features})
+
+    def Test(self, test_data):
+        """Tests the model on unseen test data.
+        
+        Args:
+            test_data: The test data. It must provide field images and a field
+                labels that give us the test features and labels.
+        """
+        y_true = tf.placeholder(tf.float32, [None, 10])
+        correct_prediction = tf.equal(tf.argmax(self.y_, 1), 
+                                      tf.argmax(y_true, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        print "Test accuracy:", self.sess_.run(accuracy, 
+                        {self.x_: test_data.images, y_true: test_data.labels})
+
+
+def ThresholdPixels(pixels, threshold=0.0001):
+    """Applies a binary threshold and converts all pixels to 0 or 1.
+    
+    Args:
+        threshold: (float) Any pixels less than the threshold are set to 0
+            and any pixels at least the threshold value are set to 1.
+    """
+    pixels[pixels < threshold] = 0
+    pixels[pixels >= threshold] = 1 
+
+
+def BuildSimpleLearner():
+    """Trains a SimplerLearner model on the MNIST dataset
+    
+    Returns:
+        learner: (SimpleLearner) The trained model.
+    """
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    learner = SimpleLearner()
+    ThresholdPixels(mnist.train.images)
+    learner.Train(mnist.train)
+    ThresholdPixels(mnist.test.images)
+    learner.Test(mnist.test)
+    return learner
+
+
+def DrawInputs():
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    digit_pixels = mnist.train.images[3,:]
+    from PIL import Image
+    img = Image.new(mode="L", size=(28,28), color="black")
+    for y in range(28):
+        for x in range(28):
+            if digit_pixels[y*28+x] > 0:
+                img.putpixel((x, y), 255)
+    img.save("digit.png", "png")
 
 def main():
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    BuildSimpleLearner()
+    #DrawInputs()
+
 
 if __name__ == "__main__": main()
--- workspace/MNIST/UI.py
+++ workspace/MNIST/UI.py
@@ -13,31 +13,40 @@ from Tkinter import N, S, E, W, CENTER
 from Tkinter import Tk
 import tkMessageBox
 
+from Model import BuildSimpleLearner
 from Model import WIDTH, HEIGHT
 
 class MainWindow(object):
-    """Main window that allows users to draw a digit and ask the digit classifer to classify it.
+    """Main window that allows users to draw a digit and classify it.
     """
 
-    def __init__(self):
+    def __init__(self, model):
         """Sets up the user interface.
+        
+        Args:
+            model: The trained model for classifying digits.
         """
+        self.model_ = model
         self.main_window_ = Tk()
         self.main_frame_ = Frame(self.main_window_)
 
-        self.canvas_ = Canvas(self.main_window_, width=WIDTH, height=HEIGHT, bg="black")
+        self.canvas_ = Canvas(self.main_window_, width=WIDTH, height=HEIGHT, 
+                              bg="black")
         self.canvas_.grid(row=0)
         self.img_ = Image.new(mode="L", size=(WIDTH, HEIGHT), color="black")
         self.img_draw_ = ImageDraw.Draw(self.img_)
         self.canvas_.bind("<B1-Motion>", self.OnMouseDragged)
+        self.canvas_.bind("<ButtonRelease-1>", self.OnMouseReleased)
         self.last_x_ = None
         self.last_y_ = None
-        self.line_width_ = 2
+        self.line_width_ = 3
 
-        self.classify_ = Button(self.main_window_, text="Classify", command=self.Classify)
+        self.classify_ = Button(self.main_window_, text="Classify", 
+                                command=self.Classify)
         self.classify_.grid(row=1, sticky=N+S+E+W)
 
-        self.clear_ = Button(self.main_window_, text="Clear", command=self.Clear)
+        self.clear_ = Button(self.main_window_, text="Clear", 
+                             command=self.Clear)
         self.clear_.grid(row=2, sticky=N+S+E+W)
 
         self.main_frame_.place(relx=0.5, rely=0.5, anchor=CENTER)
@@ -49,27 +58,43 @@ class MainWindow(object):
             event: the data associated with the drag event
         """
         if self.last_x_ is not None and self.last_y_ is not None:
-            self.canvas_.create_line(self.last_x_, self.last_y_, event.x, event.y, fill="white", width=self.line_width_)
-            self.img_draw_.line((self.last_x_, self.last_y_, event.x, event.y), fill="white", width=self.line_width_)
+            self.canvas_.create_line(self.last_x_, self.last_y_, event.x, 
+                                     event.y, fill="white", 
+                                     width=self.line_width_)
+            self.img_draw_.line((self.last_x_-self.line_width_, self.last_y_-self.line_width_, event.x-self.line_width_, event.y-self.line_width_), 
+                                fill="white", width=self.line_width_)
 
         self.last_x_, self.last_y_ = event.x, event.y
+
+    def OnMouseReleased(self, event):
+        """Handles when mouse button released on canvas.
+        """
+        self.last_x_ = None
+        self.last_y_ = None
 
     def Classify(self):
         """Classifies the digit drawn on the canvas.
         """
         print "Classifying..."
-        pixels = np.zeros((WIDTH*HEIGHT,))
+        pixels = np.zeros([1,WIDTH*HEIGHT])
+
         for y in range(HEIGHT):
             for x in range(WIDTH):
-                pixels[y*HEIGHT + x] = self.img_.getpixel((x, y))
+                pixels[0][y*HEIGHT + x] = (0 if self.img_.getpixel((x, y)) == 0 
+                                        else 1)
 
-        tkMessageBox.showinfo("Classification Result", "The classifier believes you drew a 0")
-        print pixels
+        prediction = self.model_.Predict(pixels)
+        self.img_.save("test.png", "png")
+        tkMessageBox.showinfo("Classification Result", 
+                              "The classifier believes you drew a %d" 
+                              %(prediction))
 
     def Clear(self):
         """Clears the digit drawn on the canvas.
         """
         self.canvas_.delete("all")
+        self.img_ = Image.new(mode="L", size=(WIDTH, HEIGHT), color="black")
+        self.img_draw_ = ImageDraw.Draw(self.img_)
         self.last_x_, self.last_y_ = None, None
 
     def Show(self):
@@ -79,7 +104,8 @@ class MainWindow(object):
 
 
 def main():
-    main_window = MainWindow()
+    model = BuildSimpleLearner()
+    main_window = MainWindow(model)
     main_window.Show()
 
 if __name__ == "__main__": main()
