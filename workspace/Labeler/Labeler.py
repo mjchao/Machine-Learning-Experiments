c669bbf0f6f119fcb304f96700f114387527ff6f
Mickey Chao <mjchao2@gmail.com>
2016-09-02 12:34:31 -0400

    Undo changes to scoring

--- workspace/Labeler/Labeler.py
+++ workspace/Labeler/Labeler.py
@@ -177,7 +177,7 @@ class BagOfWordsClassifier(object):
                     if bigram_id != 0:
                         score *= (self._weights[bigram_id][category] /
                                   self._word_counts[bigram_id])
-            scores[category] = score if score != 1.0 else 0.0
+            scores[category] = score
         #print np.sort(scores/scores.sum())[::-1][:10]
         best_idxs = np.argsort(scores)[::-1]
         return best_idxs[:10]
